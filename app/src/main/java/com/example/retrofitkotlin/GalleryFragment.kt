4a8d7d11a31edde50b437c4a88050ca08f2d9b40
jula1717 <julaplocienniczak@gmail.com>
2023-11-11 18:53:25 +0100

    searchView for own queries

--- app/src/main/java/com/example/retrofitkotlin/GalleryFragment.kt
+++ app/src/main/java/com/example/retrofitkotlin/GalleryFragment.kt
@@ -1,7 +1,10 @@
 package com.example.retrofitkotlin
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.LifecycleOwner
@@ -38,7 +41,36 @@ class GalleryFragment(): Fragment(R.layout.fragment_gallery) {
             photoAdapter.submitData(viewLifecycleOwner.lifecycle,it)
         }
 
+        setHasOptionsMenu(true)
+
 }
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.menu, menu)
+
+        val searchItem = menu.findItem(R.id.search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                if (query != null) {
+                    binding.recyclerView.scrollToPosition(0)
+                    viewModel.searchPhotos(query)
+                    searchView.clearFocus()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
