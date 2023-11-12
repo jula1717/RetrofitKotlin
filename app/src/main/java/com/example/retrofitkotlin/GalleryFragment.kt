c947e787175b0d63b60443962cfca58485290d94
jula1717 <julaplocienniczak@gmail.com>
2023-11-12 10:19:32 +0100

    loadStateListener

--- app/src/main/java/com/example/retrofitkotlin/GalleryFragment.kt
+++ app/src/main/java/com/example/retrofitkotlin/GalleryFragment.kt
@@ -5,9 +5,11 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.LifecycleOwner
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.retrofitkotlin.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,22 +31,41 @@ class GalleryFragment(): Fragment(R.layout.fragment_gallery) {
         binding.apply {
             recyclerView.apply {
                 adapter = photoAdapter.withLoadStateHeaderAndFooter(
-                    header = PhotoLoadStateAdapter{photoAdapter.retry()},
-                    footer = PhotoLoadStateAdapter{photoAdapter.retry()}
+                    header = PhotoLoadStateAdapter { photoAdapter.retry() },
+                    footer = PhotoLoadStateAdapter { photoAdapter.retry() }
                 )
                 layoutManager = LinearLayoutManager(requireContext())
+                recyclerView.itemAnimator = null
                 setHasFixedSize(true)
             }
         }
 
         viewModel.photos.observe(viewLifecycleOwner) {
-            photoAdapter.submitData(viewLifecycleOwner.lifecycle,it)
+            photoAdapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
 
         setHasOptionsMenu(true)
 
-}
+        photoAdapter.addLoadStateListener { loadState ->
+            binding.apply {
+                progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading
+                buttonRetry.isVisible = loadState.source.refresh is LoadState.Error
+                textViewError.isVisible = loadState.source.refresh is LoadState.Error
 
+                // empty view
+                if (loadState.source.refresh is LoadState.NotLoading &&
+                    loadState.append.endOfPaginationReached &&
+                    photoAdapter.itemCount < 1
+                ) {
+                    recyclerView.isVisible = false
+                    textViewEmpty.isVisible = true
+                } else {
+                    textViewEmpty.isVisible = false
+                }
+            }
+        }
+    }
 
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
